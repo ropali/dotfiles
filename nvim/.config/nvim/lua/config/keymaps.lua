be7ca72d17b5881c644d88a1150455e116aecb5a
Ropali Munshi <ropali68@gmail.com>
2025-07-14 14:57:08 +0000

    feat: python neotest working

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,14 +5,22 @@
 -- Run nearest test
 vim.keymap.set("n", "<leader>tn", function()
   require("neotest").run.run()
-end)
+  require("neotest").summary.open()
+end, { desc = "Run nearest test" })
 
 -- Run current file
 vim.keymap.set("n", "<leader>tf", function()
   require("neotest").run.run(vim.fn.expand("%"))
-end)
+  require("neotest").summary.open()
+end, { desc = "Run current file tests" })
 
 -- Toggle summary panel
 vim.keymap.set("n", "<leader>ts", function()
   require("neotest").summary.toggle()
-end)
+end, { desc = "Toggle test summary" })
+
+-- Run all tests
+vim.keymap.set("n", "<leader>ta", function()
+  require("neotest").run.run("tests")
+  require("neotest").summary.open()
+end, { desc = "Run all tests" })
--- nvim/.config/nvim/lua/plugins/neotest-python.lua
+++ nvim/.config/nvim/lua/plugins/neotest-python.lua
@@ -1,5 +1,24 @@
 return {
   {
-    "nvim-neotest/neotest-python",
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-python",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python")({
+            dap = { justMyCode = false },
+            runner = "pytest",
+            python = "python",
+            pytest_discover_instances = true,
+          }),
+        },
+      })
+    end,
   },
 }
