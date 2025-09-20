d9f5f4784b09ff82cd5a2438285a1d284ed540bb
Ropali Munshi <ropali68@gmail.com>
2025-09-20 21:34:46 +0530

    fix: clean up

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,27 +4,27 @@
 
 -- Run nearest test
 vim.keymap.set("n", "<leader>tn", function()
-  require("neotest").run.run()
-  require("neotest").summary.open()
+    require("neotest").run.run()
+    require("neotest").summary.open()
 end, { desc = "Run nearest test" })
 
 -- Run current file
 vim.keymap.set("n", "<leader>tf", function()
-  require("neotest").run.run(vim.fn.expand("%"))
-  require("neotest").summary.open()
+    require("neotest").run.run(vim.fn.expand("%"))
+    require("neotest").summary.open()
 end, { desc = "Run current file tests" })
 
 -- Toggle summary panel
 vim.keymap.set("n", "<leader>ts", function()
-  require("neotest").summary.toggle()
+    require("neotest").summary.toggle()
 end, { desc = "Toggle test summary" })
 
 -- Run all tests
 vim.keymap.set("n", "<leader>ta", function()
-  require("neotest").run.run("tests")
-  require("neotest").summary.open()
+    require("neotest").run.run("tests")
+    require("neotest").summary.open()
 end, { desc = "Run all tests" })
 
 vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-  require("conform").format({ async = true, lsp_fallback = true })
+    require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format file or range (in visual mode)" })
